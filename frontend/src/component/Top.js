d19c3d441ebe928a5249aff1e154dc25e52bdcd2
cmkrosp <cmkrosp@gmail.com>
2021-10-05 12:05:29 +0900

    testing

---
+++ frontend/src/component/Top.js
@@ -0,0 +1,11 @@
+import { Header} from 'semantic-ui-react'
+import Gnb from './Gnb';
+export default function Top() {
+    return (
+    <div>
+        <img src="/images/logo-social.png" alt="logo" />
+<Header as='h1'>Test Header</Header>
+<Gnb />
+        </div>
+        );
+}
